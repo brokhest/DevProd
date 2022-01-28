5ad0a8cec0b269cd78b732a7c10ca401c0fadb87
brokhest <46522218+brokhest@users.noreply.github.com>
2022-01-28 22:49:39 +1000

    deployed_prod

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -2,7 +2,7 @@ cd ../
 
 pip freeze > requirements.txt
 git add .
-git commit -m "deployed_dev"
+git commit -m "deployed_prod"
 git push 
 
 ssh -i E:/dossh/key root@167.99.245.100 'cd .. && cd home/broha/site/Production/DevProd/scripts && sh build.sh'
